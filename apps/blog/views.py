e15d51b24d3a7fbbcc9e625bbbc93fef4cbf474b
garvyPng <nastyabokatova@gmail.com>
2023-01-08 13:21:35 +0200

    tag search

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -16,3 +16,9 @@ def article_view(request, category_id, article_id):
     article = Article.objects.get(id=article_id)
     category = BlogCategory.objects.get(id=category_id)
     return render(request, 'blog/article/view.html', {'article':article,'category':category})
+
+
+def tag_search_article_list(request, tag_id):
+    tag = Tag.objects.get(id=tag_id)
+    articles = Article.objects.filter(tags__in=[tag_id])
+    return render(request, 'blog/article/tag_search.html', {'articles': articles, 'tag': tag})
